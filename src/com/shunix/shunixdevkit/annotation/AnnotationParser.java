bd3c0668cee3217dc6e49b1678eefbe5bec31baf
Shunix <admin@shunix.com>
2013-12-15 05:36:19 +0800

    Add annotation support for fragments

--- src/com/shunix/shunixdevkit/annotation/AnnotationParser.java
+++ src/com/shunix/shunixdevkit/annotation/AnnotationParser.java
@@ -3,6 +3,7 @@ package com.shunix.shunixdevkit.annotation;
 import java.lang.reflect.Field;
 
 import android.app.Activity;
+import android.view.View;
 
 /**
  * To use annotation to init all view components. You should init a annotation
@@ -10,7 +11,7 @@ import android.app.Activity;
  * 
  * @author Ray WANG
  * @since Dec 13th
- * @version 1.0.0
+ * @version 1.1.0
  */
 public class AnnotationParser {
 	private static AnnotationParser parser;
@@ -43,6 +44,27 @@ public class AnnotationParser {
 		try {
 			field.setAccessible(true);
 			field.set(activity, activity.findViewById(viewId));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** Use Reflection to init all component in an activity */
+	public void initAllComponent(android.support.v4.app.Fragment fragment, View view) {
+		Field[] fields = fragment.getClass().getDeclaredFields();
+		for (Field field : fields) {
+			if (field.isAnnotationPresent(ViewComponent.class)) {
+				initComponent(fragment, view, field);
+			}
+		}
+	}
+
+	private void initComponent(android.support.v4.app.Fragment fragment, View view, Field field) {
+		ViewComponent viewComponent = field.getAnnotation(ViewComponent.class);
+		int viewId = viewComponent.id();
+		try {
+			field.setAccessible(true);
+			field.set(fragment, view.findViewById(viewId));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
