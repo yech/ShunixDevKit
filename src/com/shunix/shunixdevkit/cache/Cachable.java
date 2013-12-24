64971b0e40e16ad8c0778ee0ea66fb3da83edca8
Shunix <admin@shunix.com>
2013-12-24 17:08:14 +0800

    Add some comment to show how the cachable interface will be used

--- src/com/shunix/shunixdevkit/cache/Cachable.java
+++ src/com/shunix/shunixdevkit/cache/Cachable.java
@@ -1,5 +1,14 @@
 package com.shunix.shunixdevkit.cache;
 
+/**
+ * All classes to be cached need to implement this interface. The getName()
+ * method will return the table name of this table. Usually, returing the class
+ * name will be OK.
+ * 
+ * @author Ray WANG <admin@shunix.com>
+ * @since Dec 22nd, 2013
+ * @version 1.0.0
+ */
 public interface Cachable {
 	public String getName();
 }
